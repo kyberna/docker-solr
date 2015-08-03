d00d73c0bd7b8f9d797db25802b72936409ad496
kse <sebastian.koehlmeier@kyberna.com>
2015-08-03 15:17:40 +0200

    Fixed SOLR_MEM Env Usage in startup script.

--- run.sh
+++ run.sh
@@ -4,9 +4,8 @@ if [ -e /solr/init.sh ];then
 	source /solr/init.sh
 fi
  
-if [ ! -f /etc/container_environment/SOLR_MEM ]; then
+if [ ! $SOLR_MEM ]; then
 	SOLR_MEM=1g
-	echo $SOLR_MEM > /etc/container_environment/SOLR_MEM
 fi
 
 /opt/solr/bin/solr -f -a $SOLR_OPTS -m $SOLR_MEM
